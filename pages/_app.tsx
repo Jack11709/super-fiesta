ba7f53165ea7d684e6f4144e30678716b0311f0c
Jack <jack.may@generalassemb.ly>
2021-06-08 14:56:42 +0100

    temporary page built

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
 
-function NextApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function NextApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default NextApp
+const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box;
+    height: 100%;
+    margin:0 auto;
+    padding: 0;
+    font-family: 'Roboto Mono', monospace;
+  }
+
+  body{
+    min-height:100vh;
+    margin: 0;
+  }
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
+import styled from 'styled-components'
+
 export default function Home() {
   return (
-    <div>
-      The Bagel Man
-    </div>
+    <HomeContainer>
+      <HomeContent>
+        <h1>Do you know the Bagel Man?</h1>
+        <h2>Coming Soon</h2>
+      </HomeContent>
+    </HomeContainer>
   )
 }
+
+const HomeContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url('/splash.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: #4f4f4f;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const HomeContent = styled.div`
+  height: 65%;
+  color: white;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
